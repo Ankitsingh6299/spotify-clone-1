f23f40070d6851656819a800b2ff87b1623c529b
Ankitsingh6299 <ankitsingh6299@gmail.com>
2022-04-18 23:48:46 +0530

    Added body,sidebar etc

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Login from './components/login/login';
 import {getTokenFromUrl} from './components/spotify';
 import SpotifyWebApi from 'spotify-web-api-js';
-function App() {
-  const [token,setToken] = useState(null);
+import Player from './Player';
+import { useStateValue } from "./StateProvider";
+
+  
 const spotify = new SpotifyWebApi();
+
+function App() {
+  
+  const[{user , token},dispatch] = useStateValue();
   useEffect(()=>{
     const hash=getTokenFromUrl();
     window.location.hash="";
     const _token=hash.access_token;
     if(_token){
-      setToken(_token);
+
+      dispatch({
+        type:"SET_TOKEN",
+        token:_token,
+      })
+     
       spotify.setAccessToken(_token);
 
       spotify.getMe().then((user)=>{
-        console.log("User: ",user); 
+        
+
+        dispatch({
+          type:"SET_USER",
+          user:user,
+        });
       })
-      console.log("The token is:",_token);
+      
     }
-  })
+    
+  },[]);
+  
+
   return (
     <div className="app">
-      {token?(<h1>Logged in</h1>):(<Login />)}
+      {token?(<Player spotify={spotify} />):(<Login />)}
     
     </div>
     
---
+++ src/Body.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Body.css';
+function Body() {
+  return (
+    <div className='body'>I'm the Body</div>
+  )
+}
+
+export default Body
